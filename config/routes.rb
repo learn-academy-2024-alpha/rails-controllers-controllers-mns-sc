752b06da011c77764e202aeac0d955a10abb2647
smorgannicole <morgann.smithh@yahoo.com>
2024-03-26 16:50:18 -0700

    params challenges

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/festivals', to: 'home#festivals', as: 'festivals'
   get '/nature', to: 'home#nature', as: 'nature'
   root 'home#landing'
+  get '/cubed', to: 'main#cubed'
+  get '/evenly', to: 'main#evenly'
+  get '/palindrome', to: 'main#palindrome'
 end
