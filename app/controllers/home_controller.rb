d2b681b8714cd58e19f1893d37232f44a98a6a53
sethcrist <sethcrist50@gmail.com>
2024-03-26 14:56:27 -0700

    added some styling and 3 other pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+    def landing 
+    end
+
+    def attractions
+    end
+
+    def food
+    end
+
+    def festivals
+    end
+
+    def nature
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/landing', to: 'home#landing', as: 'landing'
+  get '/attractions', to: 'home#attractions', as: 'attractions'
+  get '/food', to: 'home#food', as: 'food'
+  get '/festivals', to: 'home#festivals', as: 'festivals'
+  get '/nature', to: 'home#nature', as: 'nature'
+  root 'home#landing'
 end
